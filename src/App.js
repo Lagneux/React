dade0d4ebfa207a4ecba02c75500ef427220e1cd
Lagneux <sorolagnonaristide@gmail.com>
2023-03-20 15:50:36 +0000

    Checkpoint React - Ajout d'un formulaire

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import React from 'react';
 import logo from './logo.svg';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Bienvenue à React!</h1>
+        <p>Identifiez-vous!</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      <form>
+        <div class="row align-items-center g-3">
+        <div class="col-md-auto col-12">
+            <label class="form-label d-md-none" for="inputEmail">Email</label>
+            <input type="email" class="form-control" id="inputEmail" placeholder="Email" />
+        </div>
+        <div class="col-md-auto col-12">
+            <label class="form-label d-md-none" for="inputPassword">Password</label>
+            <input type="password" class="form-control" id="inputPassword" placeholder="Password" />
+        </div>
+        <div class="col-md-auto col-12">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="checkRemember" />
+                <label class="form-check-label" for="checkRemember">Remember me</label>
+            </div>
+        </div>
+        <div class="col-md-auto col-12">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+    </div>
+</form>
       </header>
+      
     </div>
   );
 }
